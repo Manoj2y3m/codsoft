a1f2344b67bf1162391df7569d974b50a52b29bd
Manoj2y3m <mrmanojkumar335577@gmail.com>
2026-02-04 07:38:08 +0530

    Update and rename Bank account.java to ATMInterface.java
    Signed-off-by: Manoj2y3m <mrmanojkumar335577@gmail.com>

---
+++ ATMInterface.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class ATMInterface {
+
+    static double balance = 5000; // Initial balance
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        System.out.println("Welcome to ATM Interface");
+
+        do {
+            System.out.println("\n===== ATM MENU =====");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.print("Choose option: ");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Current Balance: ₹" + balance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter deposit amount: ");
+                    double deposit = sc.nextDouble();
+                    if (deposit > 0) {
+                        balance += deposit;
+                        System.out.println("Amount deposited successfully.");
+                    } else {
+                        System.out.println("Invalid amount.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter withdrawal amount: ");
+                    double withdraw = sc.nextDouble();
+                    if (withdraw > 0 && withdraw <= balance) {
+                        balance -= withdraw;
+                        System.out.println("Please collect your cash.");
+                    } else {
+                        System.out.println("Insufficient or invalid balance.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Thank you for using ATM. Visit again!");
+                    break;
+
+                default:
+                    System.out.println(" Invalid choice. Try again.");
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
